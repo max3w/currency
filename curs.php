37b255cd78d1f07de9eb3191dd0b03e3062d2234
m.alekseenko <m.alekseenko@skywell.software>
2022-08-15 15:41:22 +0300

    Fix v2 currency

--- curs.php
+++ curs.php
@@ -3,14 +3,13 @@
 function curs()
     
 {
-    $cache_time_out = 10800; // Время жизни кэша в секундах
+    $cache_time_out = 1; // Время жизни кэша в секундах
     $file_currency_cache = 'currency.txt'; // Файл кэша
 
     if (!is_file($file_currency_cache) || filemtime($file_currency_cache) < (time() - $cache_time_out)) {
         $url = "https://kurs.com.ua/valyuta/usd";
-        $file_currency_cache = 'currency.txt'; // Файл кэша
         $curs = file_get_contents($url);
-        $curs = preg_match_all('#<script type="application/ld\+json">(.+?)</script>#is', $curs, $arr); // Парсинг выбраного фида
+        preg_match_all('#<script type="application/ld\+json">(.+?)</script>#is', $curs, $arr); // Парсинг выбраного фида
         $arr = $arr[1][0];
         $curs = json_decode($arr, true);
         $curs = $curs['mainEntity']['csvw:tableSchema']['csvw:columns'][3]['csvw:cells'][0]['csvw:value'];
